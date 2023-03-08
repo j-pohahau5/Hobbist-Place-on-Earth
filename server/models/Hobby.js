f7687f5ab7bc258e1e89f9ed380a40b3a9d4953f
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-08 15:18:09 -0500

    hobby model

---
+++ server/models/Hobby.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const hobbySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 20,
+    },
+    description: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 10,
+        maxlength: 500,
+    },
+    categoryID: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Hobby',
+        },
+    ],
+    userID: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+});
+
+const Hobby = model('Hobby', hobbySchema);
+
+module.exports = Hobby;
+
+// Title, description, category_Id, Users_ID
+// UUID 
+// Has Title, description, category; length validation on title and description
+// Has many users,
+// Has many likes,
+// Has many comments
