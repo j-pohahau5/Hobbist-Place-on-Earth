161e4890b000e19b117fd29e4a5858542faaab5f
Jonathan Pohahau <j.pohahau@outlook.com>
2023-03-08 11:00:39 -0800

    seed jsonfiles completed will need to adjust once models are done

---
+++ server/seeders/comment.js
@@ -0,0 +1,42 @@
+[  
+  {    
+    "content": "I love drawing too!",   
+    "hobbyId": "1",    
+    "userId": "2"  
+  },  
+  {    
+    "content": "Great shot!",    
+    "hobbyId": "2",    
+    "userId": "1"  
+  },  
+  {    
+    "content": "You're really talented!",   
+    "hobbyId": "3",    
+    "userId": "1"  
+  },  
+  {    
+    "content": "Coding is so fun!",    
+    "hobbyId": "4",    
+    "userId": "2"  
+  },  
+  {    
+    "content": "Your drawings are amazing!",    
+    "hobbyId": "1",    
+    "userId": "3"  
+  },  
+  {    
+    "content": "I used to play basketball too!",    
+    "hobbyId": "2",    
+    "userId": "3"  
+  },   
+  {   
+    "content": "I wish I could play guitar like you!",    
+    "hobbyId": "3",     
+    "userId": "2"  
+  },  
+  {    
+    "content": "I'm learning to code too!",    
+    "hobbyId": "4",    
+    "userId": "3"  
+  }
+]
