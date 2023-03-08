fdd8176cea7aade8351ebb6f1b974fd7e6dbf2d1
Jonathan Pohahau <j.pohahau@outlook.com>
2023-03-08 11:27:34 -0800

    added requires for index.js Model amd exporting it

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Category = require('./Category');
+const Hobby = require('./Hobby');
+const Comment = require('./Comment');
+
+module.exports = { User, Category, Hobby, Comment };
