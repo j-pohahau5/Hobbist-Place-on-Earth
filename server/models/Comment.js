4f75efcd8ed1e13f970d31ace8a736af95f59037
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-08 14:20:44 -0500

    comment model

---
+++ server/models/Comment.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema = new Schema({
+    content: {
+        type: String, 
+        required: true, 
+        unique: true,
+        minlength: 10,
+        maxlength: 500,
+    },
+    userId: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    HobbyId: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Hobby',
+        },
+    ],
+});
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
+
