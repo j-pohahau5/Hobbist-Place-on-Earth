47817304a041de607e682debdc14a1076812e1f6
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-10 15:25:38 -0500

    signup function added

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,5 +2,22 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/mutations';
+import Auth from '../utils/auth';
 
+const Signup = () => {
+    const [formState, setFormState] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+    const [addUser, { error, data }] = useMutation(ADD_USER);
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+}
