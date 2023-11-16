8b29985489c98c808b3bdaeb7845caa6a7bfb985
Jonathan Pohahau <j.pohahau@outlook.com>
2023-11-15 19:54:01 -0800

    fixing server connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hobbies',
+  process.env.MONGODB_URI || 'mongodb+srv://jonathanp:IWZyk7HMxNrj4j9w@cluster0.0o3nh.mongodb.net/',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
-
+// mongodb://127.0.0.1:27017/hobbies
+// IWZyk7HMxNrj4j9w
 module.exports = mongoose.connection;
