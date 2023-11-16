e627acb5e15803216d35f87087f6524d0301f517
Jonathan Pohahau <j.pohahau@outlook.com>
2023-11-15 22:38:44 -0800

    updating mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://jonathanp:IWZyk7HMxNrj4j9w@cluster0.0o3nh.mongodb.net/?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://jpohahau:yTQ6h1B7lx8MExVi@hobbies.fajgo.mongodb.net/?retryWrites=true&w=majority',
+  () => {
+    console.log("connected")
+  },
+  e =>console.error(e),
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -9,6 +13,12 @@ mongoose.connect(
 );
 // mongodb://127.0.0.1:27017/hobbies
 // IWZyk7HMxNrj4j9w
+
+// jpohahau
+// yTQ6h1B7lx8MExVi
+
 // mongodb+srv://jonathanp:IWZyk7HMxNrj4j9w@cluster0.0o3nh.mongodb.net/
+// mongodb+srv://jonathanp:IWZyk7HMxNrj4j9w@cluster0.0o3nh.mongodb.net/
+// mongodb+srv://jonathanp:IWZyk7HMxNrj4j9w@cluster0.0o3nh.mongodb.net/?retryWrites=true&w=majority
 // mongodb+srv://jonathanp:IWZyk7HMxNrj4j9w@cluster0.0o3nh.mongodb.net/?retryWrites=true&w=majority
 module.exports = mongoose.connection;
